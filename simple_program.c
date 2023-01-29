e9dc0f3e54991ed4672b2b4c469fee37b21b53c3
geediginjalaankitha <120474107+geediginjalaankitha@users.noreply.github.com>
2023-01-29 23:46:24 +0530

    Add files via upload

---
+++ simple_program.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a;
+int b;
+a=10;
+b=5;
+printf("multiply of two num is %d\n",a*b);
+printf("addition of two num is %d\n",a+b);
+printf("subtraction of two num is %d\n",a-b);
+printf("division of two num is %d\n",a/b);
+return;
+}
